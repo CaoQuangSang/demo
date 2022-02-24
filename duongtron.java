6a9b3fcd9edca64755c4d6a0b6bfe945090a8572
CaoQuangSang <21t1020654@husc.edu.vn>
2022-02-24 14:56:23 +0700

    Add files via upload

---
+++ duongtron.java
@@ -0,0 +1,21 @@
+
+package javaapplication3;
+import java.util.Scanner;
+public class JavaApplication3 {
+
+   
+    public static void main(String[] args) {
+        int bankinh;
+        double soPI = 3.14;
+        double v;
+        Scanner scanner = new Scanner(System.in);
+         
+    System.out.print("Nhập vào bán kính hình tròn: ");
+    bankinh = scanner.nextInt();
+    v = soPI * bankinh * 2;
+       System.out.println ("Chu  hình tròn = " + (v));
+}
+
+        
+    }
+    
